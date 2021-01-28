0a6a5f6d379b0127394c51b1e65255b83c17f368
JonSteinn <jonsteinn@gmail.com>
2021-01-28 07:50:52 +0000

    debug setup

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,4 @@
+package dht
 
 import scala.math.pow
 import scala.math.BigInt
@@ -221,64 +222,63 @@ class DHT (var nodeCount: Int, val extents: Int, val copies: Int) {
     }
 }
 
-object Main {
-    def analyze(params: HashMap[String, Int], dht: DHT) {
-        println("Do statistical analysis here")
-        // TODO: stats
-    }
+object Main extends App {
+  def analyze(params: HashMap[String, Int], dht: DHT) {
+    println("Do statistical analysis here")
+    // TODO: stats
+  }
 
-    def defaultArgs(): HashMap[String, Int] = {        
-        var defaultArgs = new HashMap[String, Int]()
-        defaultArgs.put("S", 10)
-        defaultArgs.put("E", 10000)
-        defaultArgs.put("N", 3)
-        defaultArgs.put("W", 1000000)
-        defaultArgs.put("I", 5)
-        defaultArgs.put("M", 30)
-        return defaultArgs        
-    }
+  def defaultArgs(): HashMap[String, Int] = {        
+    var defaultArgs = new HashMap[String, Int]()
+    defaultArgs.put("S", 10)
+    defaultArgs.put("E", 10000)
+    defaultArgs.put("N", 3)
+    defaultArgs.put("W", 1000000)
+    defaultArgs.put("I", 5)
+    defaultArgs.put("M", 30)
+    return defaultArgs        
+  }
 
-    def argparse(args: Array[String]): HashMap[String, Int] = {
-        var argMap = defaultArgs()   
-        def next(i: Int) {
-            if (i < args.length - 1) {
-                var value = 0
-                try {
-                    value = args(i+1).toInt
-                } catch {
-                    case e: NumberFormatException => {
-                        next(i+1)
-                        return
-                    }
-                }
-                var jump = 2
-                args(i) match {
-                    case "-S" => argMap.put("S", value)
-                    case "-E" => argMap.put("E", value)
-                    case "-N" => argMap.put("N", value)
-                    case "-W" => argMap.put("W", value)
-                    case "-I" => argMap.put("I", value)
-                    case "-M" => argMap.put("M", value)
-                    case _ => jump = 1
-                }
-                next(i + jump)
-            }
+  def argparse(args: Array[String]): HashMap[String, Int] = {
+    var argMap = defaultArgs()   
+    def next(i: Int) {
+      if (i < args.length - 1) {
+        var value = 0
+        try {
+          value = args(i+1).toInt
+        } catch {
+          case e: NumberFormatException => {
+            next(i+1)
+            return
+          }
         }
-        next(0)
-        return argMap
-    }
-
-    def main(args: Array[String]) {
-        val params = argparse(args)
-        val dht = new DHT(params.get("S"), params.get("E"), params.get("N"))
-        val maxNodes = params.get("M")
-        val writes = params.get("W")
-        val increment = params.get("I")
-        while (dht.nodeCount <= maxNodes) {
-            dht.randomWrites(writes)
-            System.exit(0)
-            analyze(params, dht)
-            dht.addNodes(increment)
+        var jump = 2
+        args(i) match {
+          case "-S" => argMap.put("S", value)
+          case "-E" => argMap.put("E", value)
+          case "-N" => argMap.put("N", value)
+          case "-W" => argMap.put("W", value)
+          case "-I" => argMap.put("I", value)
+          case "-M" => argMap.put("M", value)
+          case _ => jump = 1
         }
+        next(i + jump)
+      }
     }
+    next(0)
+    return argMap
+  }
+
+
+  val params = argparse(args)
+  val dht = new DHT(params.get("S"), params.get("E"), params.get("N"))
+  val maxNodes = params.get("M")
+  val writes = params.get("W")
+  val increment = params.get("I")
+  while (dht.nodeCount <= maxNodes) {
+    dht.randomWrites(writes)
+    System.exit(0)
+    analyze(params, dht)
+    dht.addNodes(increment)
+  }
 }
