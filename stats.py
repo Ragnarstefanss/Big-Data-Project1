ea83476837fa2d44907704a17f30f6138ef5d228
JonSteinn <jonsteinn@gmail.com>
2021-01-30 13:41:36 +0000

    black py

--- stats.py
+++ stats.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
-sns.set(rc={'figure.figsize': (12, 10)})
+
+sns.set(rc={"figure.figsize": (12, 10)})
+
 
 class ResultFile:
     def __init__(self, fname):
         self.i = 0
         self.lines = ResultFile.read_lines(fname)
 
-    def get_next(self, map_to_int = False):
+    def get_next(self, map_to_int=False):
         line = self.lines[self.i]
         self.i += 1
         return map(int, line.rstrip().split()) if map_to_int else line.rstrip()
@@ -19,11 +21,11 @@ class ResultFile:
     def __bool__(self):
         return self.i < len(self.lines)
 
-    
     @staticmethod
     def read_lines(fname):
         with open(fname) as f:
             return f.readlines()
+
 
 class Data:
     def __init__(self, rf):
@@ -45,7 +47,7 @@ class Data:
             line = rf.get_next().split()
             self.node_to_extents[int(line[0][:-1])] = list(map(int, line[1:]))
         rf.dump()
-        self.jumps = list(rf.get_next(True))  
+        self.jumps = list(rf.get_next(True))
         rf.dump()
         self.node_to_fingertable = {}
         for _ in range(self.S):
@@ -60,14 +62,12 @@ class Data:
             assert len(copies) == len(copiesSet)
             self.node_to_copies[int(line[0][:-1])] = copiesSet
 
-
-    
     @staticmethod
     def get_stat_info(lis):
         n = len(lis)
         s = sum(lis)
         avg = s / n
-        std = sum((x - avg) ** 2 / (n-1) for x in lis) ** 0.5        
+        std = sum((x - avg) ** 2 / (n - 1) for x in lis) ** 0.5
         ratios = list(map(lambda x: f"{100 * x / s:.1f}", lis))
         return f"{avg:.2f}", f"{std:.2f}", ratios
 
@@ -75,8 +75,8 @@ class Data:
         w_dist_avg, w_dist_std, w_dist_ratios = Data.get_stat_info(self.node_w_dist)
         e_dist_avg, e_dist_std, e_dist_ratios = Data.get_stat_info(self.node_e_dist)
 
-        #print("e_dist_ratios: " + str(e_dist_ratios))
-        #print("w_dist_ratios: " + str(w_dist_ratios))
+        # print("e_dist_ratios: " + str(e_dist_ratios))
+        # print("w_dist_ratios: " + str(w_dist_ratios))
 
         # Change every other column color to a different color
         def color_barlist(bar, color_1, color_2):
@@ -88,15 +88,19 @@ class Data:
 
         width = 0.40
 
-        ''''''''''''''''''''''''''''''''''''
-        '''   DISTRIBUTION  2 COLUMNS    '''
-        ''''''''''''''''''''''''''''''''''''
+        """""" """""" """""" """""" """""" """"""
+        """   DISTRIBUTION  2 COLUMNS    """
+        """""" """""" """""" """""" """""" """"""
         fig, ax = plt.subplots(nrows=1, ncols=2)
-        #fig.suptitle('Node E dist vs Node W dist -'+ str(self.iteration))
-        barlist = ax[0].bar(list(range(self.S)), self.node_e_dist, width, label="Node e dist")
-        barlist2 = ax[1].bar(list(range(self.S)), self.node_w_dist, width, label="Node w dist")
+        # fig.suptitle('Node E dist vs Node W dist -'+ str(self.iteration))
+        barlist = ax[0].bar(
+            list(range(self.S)), self.node_e_dist, width, label="Node e dist"
+        )
+        barlist2 = ax[1].bar(
+            list(range(self.S)), self.node_w_dist, width, label="Node w dist"
+        )
         plt.tight_layout()
-        ax[0].set_ylabel('Dist')
+        ax[0].set_ylabel("Dist")
         ax[0].set_xticklabels(list(range(self.S)))
         ax[1].set_xticklabels(list(range(self.S)))
         color_barlist(barlist, "teal", "turquoise")
@@ -105,49 +109,53 @@ class Data:
         ax[1].legend()
         plt.savefig(f"plots/distribution{self.iteration}.png")
 
-        ''''''''''''''''''''''''''''''''''''
-        '''            JUMPS             '''
-        ''''''''''''''''''''''''''''''''''''
+        """""" """""" """""" """""" """""" """"""
+        """            JUMPS             """
+        """""" """""" """""" """""" """""" """"""
         plt.clf()
-        #plt.title("Jumps " + str(self.iteration), fontdict={'fontsize': 24})
+        # plt.title("Jumps " + str(self.iteration), fontdict={'fontsize': 24})
         jumps = plt.hist(self.jumps, len(set(self.jumps)), color="teal")
         plt.savefig(f"plots/jumps{self.iteration}.png")
 
-
-        ''''''''''''''''''''''''''''''''''''''''''''''''''
-        '''  DISTRIBUTION  E ONE COLUMN W/ TEXT ABOVE  '''
-        ''''''''''''''''''''''''''''''''''''''''''''''''''
+        """""" """""" """""" """""" """""" """""" """""" """""" ""
+        """  DISTRIBUTION  E ONE COLUMN W/ TEXT ABOVE  """
+        """""" """""" """""" """""" """""" """""" """""" """""" ""
         plt.clf()
-        #plt.title("n_e_dist " + str(self.iteration), fontdict={'fontsize': 24})
+        # plt.title("n_e_dist " + str(self.iteration), fontdict={'fontsize': 24})
         bar_one_page = plt.bar(list(range(self.S)), self.node_e_dist, width)
 
         # Add counts above the two bar graphs
         for index, data in enumerate(self.node_e_dist):
-            plt.text(x=index, y=data+1, s=f"{e_dist_ratios[index]}", fontdict=dict(fontsize=18), ha='center')
-
+            plt.text(
+                x=index,
+                y=data + 1,
+                s=f"{e_dist_ratios[index]}",
+                fontdict=dict(fontsize=18),
+                ha="center",
+            )
         plt.tight_layout()
         color_barlist(bar_one_page, "teal", "turquoise")
         plt.savefig(f"plots/node_e_dist{self.iteration}.png")
 
-        ''''''''''''''''''''''''''''''''''''''''''''''''''
-        '''  DISTRIBUTION  W ONE COLUMN W/ TEXT ABOVE  '''
-        ''''''''''''''''''''''''''''''''''''''''''''''''''
+        """""" """""" """""" """""" """""" """""" """""" """""" ""
+        """  DISTRIBUTION  W ONE COLUMN W/ TEXT ABOVE  """
+        """""" """""" """""" """""" """""" """""" """""" """""" ""
         plt.clf()
-        #plt.title("n_w_dist " + str(self.iteration), fontdict={'fontsize': 24})
+        # plt.title("n_w_dist " + str(self.iteration), fontdict={'fontsize': 24})
         bar_one_page2 = plt.bar(list(range(self.S)), self.node_w_dist, width)
 
         # Add counts above the bar graphs
         for index, data in enumerate(self.node_w_dist):
-            plt.text(x=index, y=data+1,
-                     s=f"{w_dist_ratios[index]}", fontdict=dict(fontsize=18), ha='center')
-
+            plt.text(
+                x=index,
+                y=data + 1,
+                s=f"{w_dist_ratios[index]}",
+                fontdict=dict(fontsize=18),
+                ha="center",
+            )
         plt.tight_layout()
-        color_barlist(bar_one_page2,  "teal", "turquoise")
+        color_barlist(bar_one_page2, "teal", "turquoise")
         plt.savefig(f"plots/node_w_dist{self.iteration}.png")
-        
-        
-
-
 
     def validate(self):
         for extent in self.node_to_extents[self.node_ids[0]]:
@@ -160,11 +168,11 @@ class Data:
 
         assert sorted(self.node_ids) == self.node_ids
         assert len(self.node_ids) == len(self.node_to_extents) == self.S
-        assert all(0 <= n_id < 2**self.B for n_id in self.node_ids)
+        assert all(0 <= n_id < 2 ** self.B for n_id in self.node_ids)
 
         for n_id in self.node_ids:
             for i, f_id in enumerate(self.node_to_fingertable[n_id]):
-                z = (n_id + 2**i) % (2 ** self.B)
+                z = (n_id + 2 ** i) % (2 ** self.B)
                 if z > self.node_ids[-1] or z <= self.node_ids[0]:
                     assert f_id == self.node_ids[0]
                 else:
@@ -174,10 +182,10 @@ class Data:
                             place = j
                             break
                     assert place != -1
-                    if not (self.node_ids[place-1] < z <= self.node_ids[place]):
+                    if not (self.node_ids[place - 1] < z <= self.node_ids[place]):
                         print(f"{n_id} has invalid fingertable offset 2**{i}")
                         print(f"Points at {z} and is mapped to {f_id}\nAll nodes are")
-                        print(' '.join(map(str, self.node_ids)), flush=True)
+                        print(" ".join(map(str, self.node_ids)), flush=True)
                         assert False
         print("Fingertables valid")
 
@@ -190,8 +198,8 @@ class Data:
         print("Copies valid")
 
         print("Validation complete")
-        
-        
+
+
 def process_results(rf, validate=True, plot=True):
     while rf:
         data = Data(rf)
@@ -199,6 +207,7 @@ def process_results(rf, validate=True, plot=True):
             data.validate()
         if plot:
             data.process()
+
 
 def main():
     rf = ResultFile("results.txt")
